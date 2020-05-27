9e2d76882469f3806aae4e691be4e60e43a82a17
Bocar Diallo <bocar.diallo95@gmail.com>
2020-05-27 19:08:49 -0100

    Debut de projet

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Page Index</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/bootstrap.css">
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+
+            <article class="col-md-6">
+                <h1>Welcome to the</h1>
+                <img src="public/images/logos et icones/logo2.png" alt="">
+            </article>
+
+            <article class="col-md-6">
+                <form action="" method="post" class="form">
+                    <div class="form-control">
+                        <label for="login">Login</label>
+                        <input type="text" name="login" id="login" placeholder="Saisir votre Login">
+                    </div>
+
+                    <div class="form-control">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Saisir votre mot de passe">
+                    </div>
+
+                    <div class="btn-connexion">
+                        <button type="submit" name="connexion" class="btn btn-success">Connexion</button>
+                    </div>
+
+                    <div class="btn-inscription">
+                        <p>If you don't have an account...</p>
+                        <button type="submit" name="sign-up" class="btn btn-primary">Sign up</button>
+                    </div>
+                </form>
+            </article>
+
+        </div>
+    </div>
+</body>
+</html>
